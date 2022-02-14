e984dcb1399be86d1028b746404bca1d02e27ee5
ra-shree <caaryashree@gmail.com>
2022-02-14 13:15:13 +0545

    Seperated FileHandler to a different header and cpp file. Added MyApp header and cpp file.

---
+++ include/PeriodTracker/FileHandler.cpp
@@ -0,0 +1,18 @@
+#include <FileHandler.h>
+
+namespace FileHandler
+{
+	class periodFile
+	{
+		// retrieve the data from the monthlyPeriod object
+		// write it into a file
+		// get data from the file and pass it to monthlyPeriod Object
+		// create file or if exists append data
+		// get only the last data only
+
+	public:
+		periodFile();
+		~periodFile();
+	};
+
+}
