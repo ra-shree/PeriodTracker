8fadac2d6b97597a301a4e45b1dcdea86b4b7d88
ra-shree <caaryashree@gmail.com>
2022-04-04 15:29:14 +0545

    deleted old function declarations.

--- include/PeriodTracker/periodTracker.h
+++ include/PeriodTracker/periodTracker.h
@@ -1,34 +1,4 @@
 #include <pch.h>
 
-namespace PeriodTracker
-{
-	class monthlyPeriod
-	{
-		// get the date chosen by the user
-		// calculate the next date
-		// calculate the ovulation week
-		// create a vector of only the length of each period
-		// pass the result to the GUI
-	private:
-		// So the file system can directly enter or get data from here.
-	public:
-	};
-
-}
-
-namespace FileHandler
-{
-	class periodFile
-	{
-		// retrieve the data from the monthlyPeriod object
-		// write it into a file
-		// get data from the file and pass it to monthlyPeriod Object
-		// create file or if exists append data
-		// get only the last data only
-
-	public:
-	};
-
-}
 
 
